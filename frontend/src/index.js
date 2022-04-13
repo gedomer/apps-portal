0f061535f47cb4ca2b7f43a3aa64646c40a7e536
omergedikli <omer.gedikli@loggma.com.tr>
2022-04-13 10:22:25 +0300

    setup hot reload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,10 @@ const render = () => {
 
 render()
 
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept(() => { render() })
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
