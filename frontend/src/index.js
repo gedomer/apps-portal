e5fcbc93df420b5e7e3fd92ea322d93ed2627f5f
omergedikli <omer.gedikli@loggma.com.tr>
2022-04-14 12:30:27 +0300

    removed redundant code and files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const render = () => {
   ReactDOM.render(<App />, document.getElementById('root'))
@@ -13,8 +11,3 @@ render()
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept(() => { render() })
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
